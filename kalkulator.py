3b38d5b5dd9575754e4858e5e15db4f3fdab342e
fifmazurkiewicz <fifmazurkiewicz@gmail.com>
2021-03-05 13:55:41 +0100

    commit samego kalkulatora po uruchomieniu skryptu

---
+++ kalkulator.py
@@ -0,0 +1,20 @@
+def get_info():
+    print("Witaj, to jest prosty kalkulator")
+
+
+def dodaj(a, b):
+    wynik = a + b
+    return wynik
+
+
+def odejmij(a, b):
+    return a - b
+
+
+get_info()
+
+a = int(input())
+b = int(input())
+
+print(dodaj(a, b))
+print("Koniec programu. Papa!")
